b91954656e67c5eab390b7ed98aee6ab1be5dd62
Bichar <bichar4@gmail.com>
2018-09-29 21:01:48 +0545

    server added

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -1,20 +1,20 @@
-import {Post} from './post.model';
+import { Post } from './post.model';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 
-@Injectable({providedIn:'root'})
+@Injectable({ providedIn: 'root' })
 
-export class PostsService{
-    private posts:Post[] = [];
+export class PostsService {
+    private posts: Post[] = [];
     private postsUpdated = new Subject<Post[]>();
-    getPosts(){
+    getPosts() {
         return [...this.posts];
     }
-    getPostUpdateListener(){
+    getPostUpdateListener() {
         return this.postsUpdated.asObservable();
     }
-    addPost(title:string,content:string){
-        const post:Post = {title:title,content:content};
+    addPost(title: string, content: string) {
+        const post: Post = { title: title, content: content };
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
     }
