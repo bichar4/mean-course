47bb2607e3d12d5c25b6f95ca28f6b94f3d00cd8
Bichar <bichar4@gmail.com>
2018-10-25 22:46:23 +0545

    post model for server created

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const Post = require('./models/post');
 const app = express();
+
+mongoose.connect("mongodb+srv://bichar4:randomrandom@cluster0-hdsoc.mongodb.net/node-angular?retryWrites=true")
+.then(()=>{
+  console.log('connected to database');
+})
+.catch(()=>{
+  console.log('connection failed');
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
@@ -18,29 +30,31 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts",(req,res,next) => {
-  const posts = req.body;
-  console.log(posts);
+  const post = new Post({
+    title:req.body.title,
+    content:req.body.content
+  });
+  post.save()
+  .then()
+  .catch();
+
+  console.log(post);
   res.status(201).json({
     message:'Post added succesfully'
   });
 });
 app.use("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "fadf12421l",
-      title: "First server-side post",
-      content: "This is coming from the server"
-    },
-    {
-      id: "ksajflaj132",
-      title: "Second server-side post",
-      content: "This is coming from the server!"
-    }
-  ];
-  res.status(200).json({
-    message: "Posts fetched succesfully!",
-    posts: posts
-  });
+ Post.find()
+   .exec()
+   .then(posts => {
+     console.log(posts);
+     res.status(200).json({
+      message: "Posts fetched succesfully!",
+      posts: posts
+    });
+   })
+   .catch();
+
 });
 
 module.exports = app;
